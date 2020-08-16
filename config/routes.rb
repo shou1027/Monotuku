f383b74bc52f399b6cb266a2ffbd4ead8bcffc15
shou1027 <longword871@gmail.com>
2020-08-16 15:49:38 +0000

    Update view

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   get 'home/about'
 
   get 'home/help'
+  
+  get "/" => "home#home"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
