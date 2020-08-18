254abaf73282fe03831d8fae9a9e622628a2a74e
shou1027 <longword871@gmail.com>
2020-08-18 15:00:17 +0000

    Add about-layout

--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,13 @@ Rails.application.routes.draw do
 
   get 'home/help'
   
+  get "signup" => "users#signup"
+  
   get "/" => "home#home"
   get "timeline" => "home#timeline"
   get "tag" => "home#tag"
+  get "about" => "home#about"
+  get "help" => "home#help"
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
